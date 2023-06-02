0c9b1781003f349503e928a0ebc6d1bf8c6c77ac
James Balaraman <james.balaraman@gmail.com>
2023-06-02 15:35:19 -0600

    No sweat

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards()
+{
+    const writeCards = ["Thank you, Guadalupe, for the wonderful surprise gift!",
+    "Thank you, Ollie, for the wonderful surprise gift!",
+    "Thank you, Aki, for the wonderful surprise gift!"]
+    return writeCards;
+};
+
+function countDown(n)
+{
+    let countDownn = n;
+    while (countDownn != -1)
+    {
+        console.log(countDownn--);
+    }
+};
+
+countDown(4);
